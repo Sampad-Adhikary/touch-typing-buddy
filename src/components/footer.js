8cb3504a0d9a355439110acb7e57439fe077b129
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-06-11 00:00:24 +0530

    updated github link on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(){
     return (
             <div className="Footer">
-            <a href="https://github.com/Sampad-Adhikary/MERN-Contacts-App" target="blank"><img className="iconFoo" src="https://drive.google.com/uc?id=1htnYQsuP49RxUrKDp-PU8QNPCwpMbDY3" alt="github"/></a>
+            <a href="https://github.com/Sampad-Adhikary/touch-typing-buddy" target="blank"><img className="iconFoo" src="https://drive.google.com/uc?id=1htnYQsuP49RxUrKDp-PU8QNPCwpMbDY3" alt="github"/></a>
             <a href="https://www.linkedin.com/in/sampad-adhikary-b09051196/" target="blank"><img className="iconFoo" src="https://drive.google.com/uc?id=1MyZstP6XV_YCDcLwivj38ECej8IuZKsf" alt="linkedIn"/></a>
             <p className="linkPara" id="footerP">Designed and developed by <a className="linkPage" href="https://sampadadhikary.cyclic.app/" target="blank">Sampad Adhikary</a> ©️ 2023</p>
             </div>
